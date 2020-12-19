65325d0625e3fef44ce5ae0d6ba488c340ddd2b8
hyun6ik <fkanwm@naver.com>
2020-12-19 22:59:03 +0900

    고아 객체

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -28,7 +28,11 @@ public class JpaMain {
 
             em.persist(parent);
 
+            em.flush();
+            em.clear();
 
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
 
 
             tx.commit();
